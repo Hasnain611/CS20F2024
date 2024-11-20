96277dd13db4ccf1cc2cc71a52370e4ebfe5370e
1100102271 <1100102271@L815WD220023.edu.cbe.ab.ca>
2024-11-20 11:34:05 -0700

    11:33AM

--- chapter4/src/chapter4/QuadraticFormula.java
+++ chapter4/src/chapter4/QuadraticFormula.java
@@ -9,7 +9,7 @@ package chapter4;
 import java.util.Scanner;
 public class QuadraticFormula {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		Scanner Userinput = new Scanner(System.in);	
 	
 	
